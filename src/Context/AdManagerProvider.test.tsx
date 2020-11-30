47f8b5ec49f869dc5b0f23cf9acc029052d504ae
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2020-11-30 16:25:22 +0100

    Yieldbird GPT Components 1.0.0

---
+++ src/Context/AdManagerProvider.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AdManagerProvider } from '../Context/AdManagerProvider'
+
+import { mount } from 'enzyme'
+import { act } from 'react-dom/test-utils'
+
+describe('AdManagerSlot', () => {
+  it('renders slot properly', async () => {
+    const wrapper = mount(<AdManagerProvider uuid='test' />)
+
+    await act(async () => {
+      wrapper.update()
+    })
+
+    expect(window.Yieldbird?.cmd).toHaveLength(0)
+    expect(window.googletag?.cmd).toHaveLength(1)
+  })
+})
---
+++ src/typings.d.ts
@@ -0,0 +1,48 @@
+/**
+ * Default CSS definition for typescript,
+ * will be overridden with file-specific definitions by rollup
+ */
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+interface SvgrComponent
+  extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
+
+declare module '*.svg' {
+  const svgUrl: string
+  const svgComponent: SvgrComponent
+  export default svgUrl
+  export { svgComponent as ReactComponent }
+}
+
+interface Googletag {
+  _loaded(): void
+  cmd: googletag.CommandArray
+  defineSlot(
+    adUnitPath: string,
+    size: googletag.GeneralSize,
+    optDiv?: string
+  ): googletag.Slot
+  destroySlots(slots: googletag.Slot[]): void
+  display(divOrSlot?: string | Element | googletag.Slot): void
+  enableServices(): void
+  pubads(): googletag.PubAdsService
+  sizeMapping(): googletag.SizeMappingBuilder
+}
+
+interface Window {
+  googletag: Googletag
+  // eslint-disable-next-line camelcase
+  yb_configuration: { lazyLoad: boolean }
+  Yieldbird: Yieldbird
+}
+
+interface Yieldbird {
+  cmd: Function[]
+  setGPTTargeting(slots: googletag.Slot[]): void
+  refresh(slots?: googletag.Slot[]): void
+}
+
+declare module 'gpt-mock'
